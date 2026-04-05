b1db618438f357bf09886da85095fd7d6334d771
Kemonokko <semenluben4@gmail.com>
2026-04-05 11:29:22 +0300

    Update app.js

--- app.js
+++ app.js
@@ -119,7 +119,6 @@ window.showMyProfile = () => {
     
     document.getElementById('my-profile-section').style.display = 'block';
     document.getElementById('rating-list').style.display = 'block';
-    
     document.querySelectorAll('.nav-btn').forEach(b => b.classList.remove('active'));
     document.getElementById('btn-profile').classList.add('active');
 
