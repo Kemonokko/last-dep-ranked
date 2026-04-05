6cc1548c1317ef293ab6fe4b00d6568ba86e6bb2
Kemonokko <semenluben4@gmail.com>
2026-04-05 20:16:52 +0300

    Update app.js

--- app.js
+++ app.js
@@ -55,9 +55,8 @@ window.openProfile = async (nick) => {
     const { data: p } = await supabase.from('profiles').select('*').eq('nickname', nick).single();
     if (!p) return;
 
-const globalPos = list.findIndex(player => player.nickname === p.nickname) + 1;
-const rank = getRankByPercentile(globalPos, list.length);
-
+const globalPos = window.allPlayers.findIndex(player => player.nickname === p.nickname) + 1;
+    const rank = getRankByPercentile(globalPos, window.allPlayers.length);
     // 3. ЗАПОЛНЯЕМ ДАННЫЕ
     document.getElementById('prof-nick').innerText = p.nickname;
     document.getElementById('prof-avatar').style.backgroundImage = `url('${p.avatar_url || ''}')`;
