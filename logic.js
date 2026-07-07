a88671deee947d0797a3d596fb757a8149e7b341
Kemonokko <semenluben4@gmail.com>
2026-07-07 17:45:29 +0300

    Update logic.js

--- logic.js
+++ logic.js
@@ -1,16 +1,14 @@
 export function getRankByPercentile(position, total) {
-    // ВРЕМЕННЫЙ КОСТЫЛЬ
-    if (position === 1) {
-        return 'Дракон';
-    }
+
     const percentile = (position / total) * 100;
 
-    if (percentile <= 10) return 'S-plus';
-    if (percentile <= 20) return 'S';
-    if (percentile <= 35) return 'A-plus';
-    if (percentile <= 50) return 'A';
-    if (percentile <= 70) return 'B-plus';
-    if (percentile <= 85) return 'B';
+    if (percentile <= 3) return 'Дракон';
+    if (percentile <= 9) return 'S+';
+    if (percentile <= 18) return 'S';
+    if (percentile <= 30) return 'A+';
+    if (percentile <= 45) return 'A';
+    if (percentile <= 63) return 'B+';
+    if (percentile <= 84) return 'B';
     
     return 'C';
 }
