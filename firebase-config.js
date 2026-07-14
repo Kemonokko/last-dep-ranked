27c8865adc767baa4adf801b229805db6c6fa391
Kemonokko <semenluben4@gmail.com>
2026-07-14 13:37:15 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,3 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyA-NOn73fJkwk8mEFjKP1BT0gMTKKXl_4",
-    authDomain: "://firebaseapp.com",
-    projectId: "tactile-wars",
-    storageBucket: "://appspot.com",
-    messagingSenderId: "352492746252",
-    appId: "1:352492746252:web:ab76c2d154ffd2a47dd029",
-    measurementId: "G-CV4B3MCV9P"
-};
-
-firebase.initializeApp(firebaseConfig);
-window.db = firebase.firestore();
+if (!window.db && window.firebase) {
+    window.db = window.firebase.firestore();
+}
