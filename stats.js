e9a0d26a5c1826178da8fdb3729a0753a0f14723
Kemonokko <semenluben4@gmail.com>
2026-07-14 13:29:57 +0300

    Update stats.js

--- stats.js
+++ stats.js
@@ -1,6 +1,19 @@
+import { getApps, initializeApp } from "https://gstatic.com";
+import { getFirestore } from "https://gstatic.com";
 import { getRankByPercentile } from './logic.js';
 
-const db = window.db;
+const firebaseConfig = {
+  apiKey: "AIzaSyA-3WJn73F3kdc8nLFjHPiBTQqKTXXi_4",
+  authDomain: "://firebaseapp.com",
+  projectId: "tactile-wars",
+  storageBucket: "tactile-wars.firebasestorage.app",
+  messagingSenderId: "352492746252",
+  appId: "1:352492746252:web:a075c2d85ff5d2e97da029",
+  measurementId: "G-CV4B3WCJVP"
+};
+
+const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
+const db = getFirestore(app);
 
 function formatMatchDate(timestamp) {
     if (!timestamp) return "";
