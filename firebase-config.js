7738205d5b510ea4bdec4b682514af1ba4ed8b09
Kemonokko <semenluben4@gmail.com>
2026-07-14 09:59:08 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,15 +1,12 @@
-import { initializeApp } from "https://gstatic.com";
-import { getFirestore } from "https://gstatic.com";
-
 const firebaseConfig = {
-  apiKey: "AIzaSyA-3WJn73F3kdc8nLFjHPiBTQqKTXXXi_4",
-  authDomain: "://firebaseapp.com",
-  projectId: "tactile-wars",
-  storageBucket: "tactile-wars.firebasestorage.app",
-  messagingSenderId: "352492746252",
-  appId: "1:352492746252:web:a075c2d85ff5d2e97da029",
-  measurementId: "G-CV4B3WCJVP"
+    apiKey: "AIzaSyA-NOn73fJkwk8mEFjKP1BT0gMTKKXl_4",
+    authDomain: "://firebaseapp.com",
+    projectId: "tactile-wars",
+    storageBucket: "://appspot.com",
+    messagingSenderId: "352492746252",
+    appId: "1:352492746252:web:ab76c2d154ffd2a47dd029",
+    measurementId: "G-CV4B3MCV9P"
 };
 
-const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
+firebase.initializeApp(firebaseConfig);
+window.db = firebase.firestore();
